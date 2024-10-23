8c369243593388522d9855addcf6c7f7b76c9155
Ashraf <a4rfmagdy@gmail.com>
2024-10-23 03:34:36 +0300

    setting dismissible

--- lib/widget/custome_listview.dart
+++ lib/widget/custome_listview.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:todo_app/utils/colors.dart';
+import 'package:todo_app/extensions/space.dart';
+import 'package:todo_app/utils/strings.dart';
+import 'package:todo_app/widget/task_widget.dart';
 
 Expanded customeListview() {
     return Expanded(
@@ -11,86 +13,26 @@ Expanded customeListview() {
                     onTap: () {
                       
                     },
-                    child: AnimatedContainer(
-                      margin: const EdgeInsets.symmetric(
-                        horizontal: 16,
-                        vertical: 8,
+                    child: Dismissible(
+                      onDismissed: (_) {
+                        
+                      },
+                      direction: DismissDirection.horizontal,
+                      background:  Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Icon(Icons.delete_outline,color:Colors.grey,),
+                          8.w,
+                          const Text(MyString.deletedTask,style: TextStyle(
+                            color: Colors.grey
+                          ),)
+                        ],
                       ),
-                      duration: const Duration(milliseconds: 600),
-                      decoration: BoxDecoration(
-                          color: MyColors.primaryColor.withOpacity(0.3),
-                          borderRadius: BorderRadius.circular(8),
-                          boxShadow: [
-                            BoxShadow(
-                              blurRadius: 5,
-                              color: Colors.black.withOpacity(.1),
-                              offset: const Offset(0, 4),
-                            )
-                          ]),
-                      child: ListTile(
-                        leading: GestureDetector(
-                          onTap: () {},
-                          child: AnimatedContainer(
-                            duration: const Duration(milliseconds: 600),
-                            decoration: BoxDecoration(
-                              color: MyColors.primaryColor,
-                              shape: BoxShape.circle,
-                              border: Border.all(color: Colors.grey, width: .8),
-                            ),
-                            child: const Icon(
-                              Icons.check,
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
-                        //!Task Title
-                        title: const Padding(
-                          padding: EdgeInsets.only(left: 10),
-                          child: Text(
-                            "Done",
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ),
-                        subtitle: const Padding(
-                          padding: EdgeInsets.only(left: 8.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                "Description",
-                                style: TextStyle(
-                                    fontWeight: FontWeight.w500,
-                                    color: Colors.black54),
-                              ),
-                              Align(alignment: Alignment.centerRight,
-                                child: Column(
-                                  children: [
-                                    Text(
-                                      "Date",
-                                      style: TextStyle(
-                                        color: Colors.black45,
-                                        fontWeight: FontWeight.w600,
-                                      ),
-                                    ),
-                                    Text(
-                                      "Subdate",
-                                      style: TextStyle(
-                                        color: Colors.black45,
-                                        fontWeight: FontWeight.w600,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              )
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
+                      key: Key(index.toString()),
+                      child: taskWidget()),
                   );
                 },
               ),
             );
   }
+
