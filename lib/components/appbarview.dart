c0926b4062551483cede694a8d126fda4279d189
Ashraf <a4rfmagdy@gmail.com>
2024-10-24 03:41:01 +0300

    opend and close slider

---
+++ lib/components/appbarview.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_slider_drawer/flutter_slider_drawer.dart';
+
+class AppBarView extends StatefulWidget {
+  const AppBarView({super.key, required this.drawerKey});
+  final GlobalKey<SliderDrawerState> drawerKey;
+  @override
+  State<AppBarView> createState() => _AppBarViewState();
+}
+
+class _AppBarViewState extends State<AppBarView>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animatController;
+  bool isDrawer = false;
+
+  @override
+  void initState() {
+    animatController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    animatController.dispose();
+    super.dispose();
+  }
+
+  void onDrawer() {
+    isDrawer = !isDrawer;
+    if (isDrawer) {
+      animatController.forward();
+      widget.drawerKey.currentState!.openSlider();
+    } else {
+      animatController.reverse();
+      widget.drawerKey.currentState!.closeSlider();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      height: 130,
+      child: Row(
+        children: [
+          IconButton(
+            onPressed: onDrawer,
+            icon: AnimatedIcon(
+              icon: AnimatedIcons.menu_close,
+              progress: animatController,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/widget/home.dart
+++ lib/widget/home.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:todo_app/extensions/space.dart';
-import 'package:todo_app/utils/colors.dart';
-import 'package:todo_app/utils/strings.dart';
-import 'package:todo_app/widget/custome_fab.dart';
-import 'package:todo_app/widget/custome_lottie.dart';
-import 'package:todo_app/widget/task_widget.dart';
+import 'package:flutter_slider_drawer/flutter_slider_drawer.dart';
+import 'package:todo_app/components/appbarview.dart';
+import 'package:todo_app/components/custome_fab.dart';
+import 'package:todo_app/widget/home_body.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -14,104 +12,22 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
+  final GlobalKey<SliderDrawerState> drawer = GlobalKey<SliderDrawerState>();
   @override
   Widget build(BuildContext context) {
     var textTheme = Theme.of(context).textTheme;
     final List<int> testValues = [];
     return Scaffold(
       floatingActionButton: customeFab(),
-      body: SafeArea(
-        child: SizedBox(
-          height: double.infinity,
-          width: double.infinity,
-          child: Column(
-            children: [
-              //!Custome AppBar
-              SizedBox(
-                width: double.infinity,
-                height: 100,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const SizedBox(
-                      height: 30,
-                      width: 30,
-                      child: CircularProgressIndicator(
-                        value: 1 / 3,
-                        backgroundColor: Colors.grey,
-                        valueColor:
-                            AlwaysStoppedAnimation(MyColors.primaryColor),
-                      ),
-                    ),
-                    15.w,
-                    //!MainTitle
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          MyString.mainTitle,
-                          style: textTheme.displayMedium
-                              ?.copyWith(fontWeight: FontWeight.bold),
-                        ),
-                        3.h,
-                        //!Num Of Tasks
-                        Text(
-                          "1 of 3 task",
-                          style: textTheme.titleMedium,
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-              //!Custome Divider
-              const Center(
-                child: Divider(
-                  thickness: 2,
-                  indent: 25,
-                  endIndent: 25,
-                ),
-              ),
-              20.h,
-              //!ListView Of Tasks
-              Expanded(
-                child: testValues.isNotEmpty
-                    ? ListView.builder(
-                        itemCount: testValues.length,
-                        scrollDirection: Axis.vertical,
-                        itemBuilder: (context, index) {
-                          return GestureDetector(
-                            onTap: () {},
-                            //!Dismissible
-                            child: Dismissible(
-                                onDismissed: (_) {
-                                },
-                                direction: DismissDirection.horizontal,
-                                background: Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    const Icon(
-                                      Icons.delete_outline,
-                                      color: Colors.grey,
-                                    ),
-                                    8.w,
-                                    const Text(
-                                      MyString.deletedTask,
-                                      style: TextStyle(color: Colors.grey),
-                                    )
-                                  ],
-                                ),
-                                key: Key(index.toString()),
-                                child: taskWidget()),
-                          );
-                        },
-                      )
-                      //!lottie.asset
-                    : customeLottie(testValues),
-              ),
-            ],
-          ),
+      body: SliderDrawer(
+        key: drawer,
+        isDraggable: false,
+        animationDuration: 1000,
+        slider: Container(
+          color: Colors.red,
         ),
+        appBar: AppBarView(drawerKey: drawer,),
+        child: homeBody(textTheme, testValues),
       ),
     );
   }
